73210a4f6ea5827ae7f8d7ba2bc13bfcafae2139
Loskir <kl@sinker.ru>
2022-01-17 12:39:16 +0300

    Rewrite using plain entities Support better nesting

--- src/format.ts
+++ src/format.ts
@@ -1,46 +1,28 @@
-import * as GrammyTypes from './deps.deno.ts'
+import * as GrammyTypes from '@grammyjs/types'
 
-const neverReached = (_never: never): never => {
-  throw new Error()
+type CommonFormatType =
+  | "bold"
+  | "italic"
+  | "underline"
+  | "strikethrough"
+  | "spoiler"
+  | "code"
+
+type ArgType = string | FormattedString | any
+
+const unwrap = (child: ArgType): FormattedString => {
+  if (child instanceof FormattedString) {
+    return child
+  }
+  return new FormattedString(child.toString(), [])
 }
 
-type FormatChild = string | Format
 
-interface BaseAbstractFormat {
-  type: string
+const commonFormat = (type: CommonFormatType) => (child: ArgType): FormattedString => {
+  const c = unwrap(child)
+  console.log(c)
+  return new FormattedString(c.text, [{type, offset: 0, length: c.text.length}, ...c.entities])
 }
-
-interface AbstractFormat extends BaseAbstractFormat {
-  child: string | Format
-}
-
-type CommonFormat = AbstractFormat & {
-  type:
-    | "bold"
-    | "italic"
-    | "underline"
-    | "strikethrough"
-    | "spoiler"
-    | "code"
-}
-type LinkFormat = AbstractFormat & {
-  type: 'text_link'
-  url: string
-}
-type PreFormat = AbstractFormat & {
-  type: 'pre'
-  language?: string
-}
-
-type Format =
-  | CommonFormat
-  | LinkFormat
-  | PreFormat
-
-const commonFormat = (type: CommonFormat['type']) => (child: FormatChild) => ({
-  type,
-  child,
-})
 
 export const bold = commonFormat('bold')
 export const italic = commonFormat('italic')
@@ -48,91 +30,46 @@ export const underline = commonFormat('underline')
 export const strikethrough = commonFormat('strikethrough')
 export const spoiler = commonFormat('spoiler')
 export const code = commonFormat('code')
-export function link(child: FormatChild, url: string): LinkFormat {
-  return {
-    type: 'text_link',
-    child,
-    url,
-  }
+export function link(child: ArgType, url: string): FormattedString {
+  const c = unwrap(child)
+  return new FormattedString(c.text, [{type: 'text_link', offset: 0, length: c.text.length, url}, ...c.entities])
 }
-export function pre(child: FormatChild, language?: string) {
-  return {
-    type: 'pre',
-    child,
-    language,
-  }
+export function pre(child: ArgType, language?: string): FormattedString {
+  const c = unwrap(child)
+  return new FormattedString(c.text, [{type: 'pre', offset: 0, length: c.text.length, language}, ...c.entities])
 }
 // example util function
-export function mentionUser(child: FormatChild, userId: number) {
+export function mentionUser(child: ArgType, userId: number) {
   return link(child, `tg://user?id=${userId}`)
 }
 
 type Entity = GrammyTypes.MessageEntity
+class FormattedString {
+  text: string
+  entities: Entity[]
 
-const formatEntity = (format: Format, offset: number): {text: string, entities: Entity[]} => {
-  let entities: Entity[] = []
-
-  let text: string
-  if (typeof format.child === 'string') {
-    text = format.child
-  } else {
-    const res = formatEntity(format.child, offset)
-    text = res.text
-    entities = res.entities
-  }
-
-  const getEntity = (): Entity => {
-    const base = {
-      offset,
-      length: text.length,
-    }
-    switch (format.type) {
-      case 'bold':
-      case 'italic':
-      case 'underline':
-      case 'strikethrough':
-      case 'spoiler':
-      case 'code':
-        return {
-          ...base,
-          type: format.type,
-        }
-      case 'text_link':
-        return {
-          ...base,
-          type: 'text_link',
-          url: format.url,
-        }
-      case 'pre':
-        return {
-          ...base,
-          type: 'pre',
-          language: format.language,
-        }
-      default:
-        throw neverReached(format)
-    }
-  }
-  return {
-    text,
-    entities: [...entities, getEntity()],
+  constructor(text: string, entities: Entity[]) {
+    this.text = text
+    this.entities = entities
   }
 }
 
-export function fmt(strings: TemplateStringsArray, ...formats: Format[]): {text: string, entities: Entity[]} {
+export function fmt(strings: TemplateStringsArray, ...formats: ArgType[]): FormattedString {
   let text = ''
   let entities: Entity[] = []
 
   for (let i = 0; i < strings.length; ++i) {
     text += strings[i]
     if (i < formats.length) {
-      const {text: returnedText, entities: returnedEntities} = formatEntity(formats[i], text.length)
-      text += returnedText
-      entities = entities.concat(returnedEntities)
+      const format = formats[i]
+      console.log(format)
+      if (format instanceof FormattedString) {
+        entities = entities.concat(format.entities.map((v) => ({...v, offset: v.offset + text.length})))
+        text += format.text
+      } else {
+        text += format.toString()
+      }
     }
   }
-  return {
-    text,
-    entities,
-  }
+  return new FormattedString(text, entities)
 }
