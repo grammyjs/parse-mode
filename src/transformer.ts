699c14669d67cdcee242d3f8e0de1ab312339d40
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2023-01-31 21:17:05 +0530

    add

--- src/transformer.ts
+++ src/transformer.ts
@@ -26,9 +26,37 @@ const buildTransformer = (parseMode: string) => {
       return prev(method, payload, signal);
     }
 
+    let newPayload = { ...payload, ...{ parse_mode: normalisedParseMode } };
+
+    if (
+      method === "editMessageMedia" ||
+      method === "answerInlineQuery"
+    ) {
+      if (
+        "media" in payload &&
+        !("parse_mode" in payload.media)
+      ) {
+        newPayload.media.parse_mode = normalisedParseMode;
+      }
+      else if ("results" in payload) {
+        for (let resulto in payload.results) {
+          let result = payload.results[resulto];
+          if (
+            "input_message_content" in result &&
+            !("parse_mode" in result.input_message_content)
+          ) {
+            newPayload.results[resulto].input_message_content.parse_mode = normalisedParseMode;
+          }
+          else if (!("parse_mode" in result)) {
+            newPayload.results[resulto].parse_mode = normalisedParseMode;
+          }
+        }
+      }
+    }
+
     return prev(
       method,
-      { ...payload, ...{ parse_mode: normalisedParseMode } },
+      newPayload,
       signal,
     );
   };
