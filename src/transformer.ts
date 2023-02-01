16db8d686bca19963b71445ee4a05fc25cec7c4f
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2023-02-01 08:24:50 +0530

    cherw

--- src/transformer.ts
+++ src/transformer.ts
@@ -26,32 +26,37 @@ const buildTransformer = (parseMode: string) => {
       return prev(method, payload, signal);
     }
 
-    let newPayload = { ...payload, ...{ parse_mode: normalisedParseMode } };
+    let newPayload = payload;
 
-    if (
-      method === "editMessageMedia" ||
-      method === "answerInlineQuery"
-    ) {
-      if (
-        "media" in payload &&
-        !("parse_mode" in payload.media)
-      ) {
-        newPayload.media.parse_mode = normalisedParseMode;
-      }
-      else if ("results" in payload) {
-        for (let resulto in payload.results) {
-          let result = payload.results[resulto];
-          if (
-            "input_message_content" in result &&
-            !("parse_mode" in result.input_message_content)
-          ) {
-            newPayload.results[resulto].input_message_content.parse_mode = normalisedParseMode;
-          }
-          else if (!("parse_mode" in result)) {
-            newPayload.results[resulto].parse_mode = normalisedParseMode;
+    switch (method) {
+      case "editMessageMedia":
+        if (
+          "media" in payload &&
+          !("parse_mode" in payload.media)
+        ) {
+          newPayload.media.parse_mode = normalisedParseMode;
+        }
+      break;
+
+      case "answerInlineQuery":
+        if ("results" in payload) {
+          for (let resulto in payload.results) {
+            let result = payload.results[resulto];
+            if (
+              "input_message_content" in result &&
+              !("parse_mode" in result.input_message_content)
+            ) {
+              newPayload.results[resulto].input_message_content.parse_mode = normalisedParseMode;
+            }
+            else if (!("parse_mode" in result)) {
+              newPayload.results[resulto].parse_mode = normalisedParseMode;
+            }
           }
         }
-      }
+      break;
+
+      default:
+        newPayload = { ...payload, ...{ parse_mode: normalisedParseMode } };
     }
 
     return prev(
