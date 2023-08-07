32c5642c9ccb57b01784f4f359401aa687888461
KnightNiwrem <merwinwee@gmail.com>
2023-08-07 10:00:53 +0800

    Remove @grammyjs/types

--- src/deps.deno.ts
+++ src/deps.deno.ts
@@ -3,7 +3,4 @@ export type {
   NextFunction,
   Transformer,
 } from "https://lib.deno.dev/x/grammy@^1.0/mod.ts";
-export type {
-  MessageEntity,
-  ParseMode,
-} from "https://esm.sh/@grammyjs/types@2";
+export type { MessageEntity } from "https://lib.deno.dev/x/grammy@^1.0/types.ts";
--- src/deps.node.ts
+++ src/deps.node.ts
@@ -1,2 +1,2 @@
 export type { Context, NextFunction, Transformer } from "grammy";
-export type { MessageEntity, ParseMode } from "@grammyjs/types";
+export type { MessageEntity } from "grammy/types";
