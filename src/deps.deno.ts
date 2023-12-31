49ba35bac208536edfa6e8d4ea665ea0f7fff522
KnightNiwrem <merwinwee@gmail.com>
2023-12-31 22:26:02 +0800

    Update to 1.9.0, only depend on grammY

--- src/deps.deno.ts
+++ src/deps.deno.ts
@@ -1,9 +1,14 @@
+import type {
+  Context,
+  NextFunction,
+  Transformer,
+} from "https://lib.deno.dev/x/grammy@^1.20/mod.ts";
+
 export type {
   Context,
   NextFunction,
   Transformer,
-} from "https://lib.deno.dev/x/grammy@^1.0/mod.ts";
-export type {
-  MessageEntity,
-  ParseMode,
-} from "https://esm.sh/@grammyjs/types@2";
+};
+
+export type MessageEntity = NonNullable<NonNullable<Parameters<Context["reply"]>[1]>["entities"]>[number];
+export type ParseMode = NonNullable<NonNullable<Parameters<Context["reply"]>[1]>["parse_mode"]>;
--- src/deps.node.ts
+++ src/deps.node.ts
@@ -1,2 +1,10 @@
-export type { Context, NextFunction, Transformer } from "grammy";
-export type { MessageEntity, ParseMode } from "@grammyjs/types";
+import type { Context, NextFunction, Transformer } from "grammy";
+
+export type {
+    Context,
+    NextFunction,
+    Transformer,
+  };
+  
+  export type MessageEntity = NonNullable<NonNullable<Parameters<Context["reply"]>[1]>["entities"]>[number];
+  export type ParseMode = NonNullable<NonNullable<Parameters<Context["reply"]>[1]>["parse_mode"]>;
