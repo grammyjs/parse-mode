a9c81719c349ff49d148f83b891f93a590ea1732
KnightNiwrem <merwinwee@gmail.com>
2025-01-08 19:30:57 +0800

    First draft for 2.0

--- src/format.ts
+++ src/format.ts
@@ -1,6 +1,11 @@
 import type { MessageEntity } from "./deps.deno.ts";
 
 /**
+ * Objects that implement this interface 
+ */
+export type EntityTag = Omit<MessageEntity, "offset" | "length">;
+
+/**
  * Objects that implement this interface implement a `.toString()`
  * method that returns a `string` value representing the object.
  */
@@ -12,9 +17,12 @@ export interface Stringable {
 }
 
 /**
- * Represents the formatted string after the parsing.
+ * Objects that implement this interface implement a `.text`
+ * property that returns a `string` value of itself, and a
+ * `.entities` property that returns a `MessageEntity[]` value
+ * of itself
  */
-class FormattedString implements Stringable {
+export interface FormattedString {
   /**
    * Plain text value for this `FormattedString`
    */
@@ -24,14 +32,29 @@ class FormattedString implements Stringable {
    * Format entities for this `FormattedString`
    */
   entities: MessageEntity[];
+}
+
+/**
+ * Represents the formatted string after the parsing.
+ */
+export class Fmt implements Stringable, FormattedString {
+  /**
+   * Plain text value for this `Fmt` object
+   */
+  text: string;
 
   /**
-   * Creates a new `FormattedString`. Useful for constructing a
+   * Format entities for this `Fmt` object
+   */
+  entities: MessageEntity[];
+
+  /**
+   * Creates a new `Fmt`. Useful for constructing a
    * `FormattedString` from user's formatted message
    *
    * ```ts
-   * // Constructing a new `FormattedString` from user's message
-   * const userMsg = new FormattedString(ctx.message.text, ctx.entities());
+   * // Constructing a new `Fmt` from user's message
+   * const userMsg = new Fmt(ctx.message.text, ctx.entities());
    * ```
    *
    * @param text Plain text value
@@ -43,145 +66,134 @@ class FormattedString implements Stringable {
   }
 
   /**
-   * Returns the string representation of this object
+   * Returns the string representation of this `Fmt` object
    */
   toString() {
     return this.text;
   }
 }
 
-const unwrap = (stringLike: Stringable): FormattedString => {
-  if (stringLike instanceof FormattedString) {
-    return stringLike;
-  }
-  return new FormattedString(stringLike.toString(), []);
-};
-
-const buildFormatter = <T extends Array<unknown> = never>(
+function buildFormatter<T extends Array<unknown> = never>(
   type: MessageEntity["type"],
   ...formatArgKeys: T
-) => {
-  return (stringLike: Stringable, ...formatArgs: T) => {
-    const formattedString = unwrap(stringLike);
+): (...formatArgs: T) => EntityTag {
+
+  return (...formatArgs) => {
     const formatArgObj = Object.fromEntries(
       formatArgKeys.map((formatArgKey, i) => [formatArgKey, formatArgs[i]]),
     );
-    return new FormattedString(
-      formattedString.text,
-      [{
-        type,
-        offset: 0,
-        length: formattedString.text.length,
-        ...formatArgObj,
-      }, ...formattedString.entities],
-    );
+    return { type, ...formatArgObj };
   };
 };
 
 // === Native entity functions
 /**
- * Formats the `Stringable` as bold. Incompatible with `code` and `pre`.
- * @param stringLike The `Stringable` to format.
+ * Alias for `bold`` entity tag. Incompatible with `code` and `pre`.
  */
-const bold = buildFormatter("bold");
+export function b() {
+  return buildFormatter("bold")();
+}
 /**
- * Formats the `Stringable` as inline code. Cannot be combined with any other formats.
- * @param stringLike The `Stringable` to format.
+ * `bold`` entity tag. Incompatible with `code` and `pre`.
  */
-const code = buildFormatter("code");
+export function bold() {
+  return buildFormatter("bold")();
+}
 /**
- * Formats the `Stringable` as italic. Incompatible with `code` and `pre`.
- * @param stringLike The `Stringable` to format.
+ * Alias for `italic` entity tag. Incompatible with `code` and `pre`.
  */
-const italic = buildFormatter("italic");
+export function i() {
+  return buildFormatter("italic")();
+}
 /**
- * Formats the `Stringable` as a link. Incompatible with `code` and `pre`.
- * @param stringLike The `Stringable` to format.
+ * `italic` entity tag. Incompatible with `code` and `pre`.
+ */
+export function italic() {
+  return buildFormatter("italic")();
+}
+/**
+ * Alias for `strikethrough` entity tag. Incompatible with `code` and `pre`.
+ */
+export function s() {
+  return buildFormatter("strikethrough")();
+}
+/**
+ * `strikethrough` entity tag. Incompatible with `code` and `pre`.
+ */
+export function strikethrough() {
+  return buildFormatter("strikethrough")();
+}
+/**
+ * Alias for `underline` entity tag. Incompatible with `code` and `pre`.
+ */
+export function u() {
+  return buildFormatter("underline")();
+}
+/**
+ * `underline` entity tag. Incompatible with `code` and `pre`.
+ */
+export function underline() {
+  return buildFormatter("underline")();
+}
+
+/**
+ * Alias for `link` entity tag. Incompatible with `code` and `pre`.
  * @param url The URL to link to.
  */
-const link = buildFormatter<[url: string]>("text_link", "url");
+export function a(url: string) {
+  return buildFormatter<[url: string]>("text_link", "url")(url);
+}
 /**
- * Formats the `Stringable` as a code block. Cannot be combined with any other formats.
- * @param stringLike The `Stringable` to format.
+ * `link` entity tag. Incompatible with `code` and `pre`.
+ * @param url The URL to link to.
+ */
+export function link(url: string) {
+  return buildFormatter<[url: string]>("text_link", "url")(url);
+}
+
+/**
+ * `code` entity tag. Cannot be combined with any other formats.
+ */
+export function code() {
+  return buildFormatter("code")();
+}
+/**
+ * `pre` entity tag. Cannot be combined with any other formats.
  * @param language The language of the code block.
  */
-const pre = buildFormatter<[language: string]>("pre", "language");
-/**
- * Formats the `Stringable` as a spoiler. Incompatible with `code` and `pre`.
- * @param stringLike The `Stringable` to format.
- */
-const spoiler = buildFormatter("spoiler");
-/**
- * Formats the `Stringable` as a blockquote. Cannot be nested.
- * @param stringLike The `Stringable` to format.
- */
-const blockquote = buildFormatter("blockquote");
-/**
- * Formats the `Stringable` as an expandable blockquote. Cannot be nested.
- * @param stringLike The `Stringable` to format.
- */
-const expandableBlockquote = buildFormatter("expandable_blockquote");
-/**
- * Formats the `Stringable` as a strikethrough. Incompatible with `code` and `pre`.
- * @param stringLike The `Stringable` to format.
- */
-const strikethrough = buildFormatter("strikethrough");
-/**
- * Formats the `Stringable` as a underline. Incompatible with `code` and `pre`.
- * @param stringLike The `Stringable` to format.
- */
-const underline = buildFormatter("underline");
-
-// Utility functions
+export function pre(language: string) {
+  return buildFormatter<[language: string]>("pre", "language")(language);
+}
 
 /**
- * Formats the `Stringable` as an internal Telegram link to a user. Incompatible with `code` and `pre`.
- * @param stringLike The `Stringable` to format.
- * @param userId The user ID to link to.
+ * `spoiler` entity tag. Incompatible with `code` and `pre`.
  */
-const mentionUser = (stringLike: Stringable, userId: number) => {
-  return link(stringLike, `tg://user?id=${userId}`);
-};
+export function spoiler() {
+  return buildFormatter("spoiler")();
+}
 
 /**
- * Inserts a custom emoji.
- * @param placeholder A placeholder emoji
- * @param emoji The custom emoji identifier
+ * `blockquote` entity tag. Cannot be nested.
  */
-const customEmoji = (placeholder: Stringable, emoji: string) => {
-  return link(placeholder, `tg://emoji?id=${emoji}`);
-};
-
+export function blockquote() {
+  return buildFormatter("blockquote")();
+}
 /**
- * Formats the `Stringable`` as a Telegram link to a chat message. Incompatible with `code` and `pre`.
- * @param stringLike The `Stringable` to format.
- * @param chatId The chat ID to link to.
- * @param messageId The message ID to link to.
+ * `expandable_blockquote` entity tag. Cannot be nested.
  */
-const linkMessage = (
-  stringLike: Stringable,
-  chatId: number,
-  messageId: number,
-) => {
-  if (chatId > 0) {
-    console.warn(
-      "linkMessage can only be used for supergroups and channel messages. Refusing to transform into link.",
-    );
-    return stringLike;
-  } else if (chatId < -1002147483647 || chatId > -1000000000000) {
-    console.warn(
-      "linkMessage is not able to link messages whose chatIds are greater than -1000000000000 or less than -1002147483647 at this moment. Refusing to transform into link.",
-    );
-    return stringLike;
-  } else {
-    return link(
-      stringLike,
-      `https://t.me/c/${(chatId + 1000000000000) * -1}/${messageId}`,
-    );
-  }
-};
+export function expandableBlockquote() {
+  return buildFormatter("expandable_blockquote")();
+}
 
 // ===  Format tagged template function
+
+function isFormattedString(value: unknown): value is FormattedString {
+  return typeof value === "object"
+    && value !== null
+    && "text" in value
+    && "entities" in value
+    && Array.isArray(value.entities);
+}
 
 /**
  * This is the format tagged template function. It accepts a template literal
@@ -209,47 +221,97 @@ const linkMessage = (
  * await ctx.editMessageText(cart.text, { entities: cart.entities });
  * ```
  *
- * @param rawStringParts An array of `string` parts found in the tagged template (can also be `Stringable`s)
- * @param stringLikes An array of `Stringable`s found in the tagged template
+ * @param rawStringParts An array of `string` parts found in the tagged template
+ * @param entityTagsOrFormattedStrings An array of `EntityTag`s, 
+ * `FormattedString`s, or nullary functions returning `EntityTag`s found in the tagged template
  */
-const fmt = (
-  rawStringParts: TemplateStringsArray | Stringable[],
-  ...stringLikes: Stringable[]
-): FormattedString => {
+export function fmt(
+  rawStringParts: TemplateStringsArray,
+  ...entityTagsOrFormattedStrings: (Stringable | EntityTag | (() => EntityTag))[]
+): FormattedString {
   let text = "";
   const entities: MessageEntity[] = [];
+  const openEntitiesStack: (EntityTag & { offset: number })[] = [];
 
-  const length = Math.max(rawStringParts.length, stringLikes.length);
-  for (let i = 0; i < length; i++) {
-    for (const stringLike of [rawStringParts[i], stringLikes[i]]) {
-      if (stringLike instanceof FormattedString) {
-        entities.push(
-          ...stringLike.entities.map((e) => ({
-            ...e,
-            offset: e.offset + text.length,
-          })),
-        );
-      }
-      if (stringLike != null) text += stringLike.toString();
+  for (let i = 0; i < rawStringParts.length; i++) {
+    text += rawStringParts[i];
+    if (i === rawStringParts.length - 1) {
+      break;
     }
+
+    const entityTagOrFormattedString = entityTagsOrFormattedStrings[i];
+    if (isFormattedString(entityTagOrFormattedString)) {
+      entities.push(
+        ...entityTagOrFormattedString.entities.map(e => ({
+          ...e,
+          offset: e.offset + text.length,
+        }))
+      )
+      text += entityTagOrFormattedString.text;
+      continue;
+    }
+
+    let entityTag: EntityTag | undefined;
+    if (typeof entityTagOrFormattedString === "object" && "type" in entityTagOrFormattedString) {
+      entityTag = entityTagOrFormattedString;
+    }
+    if (typeof entityTagOrFormattedString === "function") {
+      entityTag = entityTagOrFormattedString();
+    }
+    if (entityTag) {
+      const lastOpenedEntityTag = openEntitiesStack.pop();
+      if (!lastOpenedEntityTag || lastOpenedEntityTag.type !== entityTag.type) {
+        openEntitiesStack.push(...[lastOpenedEntityTag, { ...entityTag, offset: text.length }].filter(e => !!e));
+      } else {
+        entities.push({ ...lastOpenedEntityTag, length: text.length - lastOpenedEntityTag.offset } as MessageEntity);
+      }
+      continue;
+    }
+
+    text += entityTagOrFormattedString.toString();
   }
-  return new FormattedString(text, entities);
+  entities.push(...openEntitiesStack.map(e => ({ ...e, length: text.length - e.offset }) as MessageEntity));
+
+  return new Fmt(text, entities);
 };
 
-export {
-  blockquote,
-  bold,
-  code,
-  customEmoji,
-  expandableBlockquote,
-  fmt,
-  FormattedString,
-  italic,
-  link,
-  linkMessage,
-  mentionUser,
-  pre,
-  spoiler,
-  strikethrough,
-  underline,
+// Utility functions
+/**
+ * Formats the `Stringable` as an internal Telegram link to a user. Incompatible with `code` and `pre`.
+ * @param stringLike The `Stringable` to format.
+ * @param userId The user ID to link to.
+ */
+export function mentionUser(stringLike: Stringable, userId: number) {
+  return fmt`${a(`tg://user?id=${userId}`)}${stringLike}${a}`;
+}
+
+/**
+ * Inserts a custom emoji.
+ * @param placeholder A placeholder emoji
+ * @param emoji The custom emoji identifier
+ */
+export function customEmoji(placeholder: Stringable, emoji: string) {
+  return fmt`${a(`tg://emoji?id=${emoji}`)}${placeholder}${a}`;
 };
+
+/**
+ * Formats the `Stringable`` as a Telegram link to a chat message. Incompatible with `code` and `pre`.
+ * @param stringLike The `Stringable` to format.
+ * @param chatId The chat ID to link to.
+ * @param messageId The message ID to link to.
+ */
+export function linkMessage(stringLike: Stringable, chatId: number, messageId: number) {
+  if (chatId > 0) {
+    console.warn(
+      "linkMessage can only be used for supergroups and channel messages. Refusing to transform into link.",
+    );
+    return fmt`${stringLike}`;
+  } else if (chatId < -1002147483647 || chatId > -1000000000000) {
+    console.warn(
+      "linkMessage is not able to link messages whose chatIds are greater than -1000000000000 or less than -1002147483647 at this moment. Refusing to transform into link.",
+    );
+    return fmt`${stringLike}`;
+  } else {
+    return fmt`${a(`https://t.me/c/${(chatId + 1000000000000) * -1}/${messageId}`)}${stringLike}${a}`;
+  }
+}
