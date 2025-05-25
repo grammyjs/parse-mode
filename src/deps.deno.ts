b956d6e8e000398c93a49d8d0a19bc7cf26e5262
KnorpelSenf <shtrog@gmail.com>
2025-05-25 20:06:26 +0200

    refactor: drop needless imports

--- src/deps.deno.ts
+++ src/deps.deno.ts
@@ -1,31 +1,3 @@
-import type {
-  Context,
-  NextFunction,
-  Transformer,
-} from "https://lib.deno.dev/x/grammy@^1.20/mod.ts";
-
-import type {
-  InputMedia,
-  InputMediaAudio,
-  InputMediaDocument,
-  InputMediaPhoto,
-  InputMediaVideo,
-} from "https://lib.deno.dev/x/grammy@^1.20/types.ts";
-
 export type {
-  Context,
-  InputMedia,
-  InputMediaAudio,
-  InputMediaDocument,
-  InputMediaPhoto,
-  InputMediaVideo,
-  NextFunction,
-  Transformer,
-};
-
-export type MessageEntity = NonNullable<
-  NonNullable<Parameters<Context["reply"]>[1]>["entities"]
->[number];
-export type ParseMode = NonNullable<
-  NonNullable<Parameters<Context["reply"]>[1]>["parse_mode"]
->;
+  MessageEntity,
+} from "https://lib.deno.dev/x/grammy@^1.20/types.ts";
